e0ee74b9b7939c413ec6aaef1a2ff50707e86f7c
adnan kursun <Adnankursun@bastion-fuchicorp-com.us-central1-a.c.geometric-sled-282503.internal>
2020-09-07 01:33:05 +0000

    added files

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -1,33 +1,43 @@
-# module "academy-deploy" {
-#   source  = "fuchicorp/chart/helm"
-#   deployment_name        = "hello-world"
-#   deployment_environment = "${var.deployment_environment}"
-#   deployment_endpoint    = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}.${var.google_domain_name}"
-#   deployment_path        = "hello-world"
-#   template_custom_vars  = {     
-#     deployment_image     = "${var.deployment_image}"
-#   }
-# }
+module "academy-deploy" {
+  source  = "fuchicorp/chart/helm"
+  deployment_name        = "hello-world"
+  deployment_environment = "${var.deployment_environment}"
+  deployment_endpoint    = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}.${var.google_domain_name}"
+  deployment_path        = "hello-world"
+  credentials            = "${var.credentials}"
+  template_custom_vars  = {     
+    deployment_image     = "${var.deployment_image}"
+   }
+  
+ }
+ 
 output "application_endpoint" {
     value = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}.${var.google_domain_name}"
 }
 variable  "deployment_image" {
     default = "docker.fuchicorp.com/hello-world-app-dev-feature:4b7aacb"
 }
+
 variable "deployment_environment" {
     default = "stage"
 }
+
 variable "deployment_endpoint" {
     type = "map"
      default = {
-        test  = "test.hello"
-        dev  = "dev.hello"
-        qa   = "qa.hello"
-        prod = "hello"
-        stage = "stage.hello"
-  }
+        test                 = "test.data-miner"
+        dev                  = "dev.data-miner"
+        qa                   = "qa.data-miner"
+        prod                 = "data-miner"
+        stage                = "stage.data-miner"
+ }
 }
+
 variable "google_domain_name" {
     default = "fuchicorp.com"
 }
 
+variable "credentials" {
+    default = "common-service-account.json"
+    description = "- (Optional) Google Service account Json file."
+}
