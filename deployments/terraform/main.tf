d34697a6e8a9c1c3036f1f013647f84523346eb8
adnan kursun <Adnankursun@bastion-fuchicorp-com.us-central1-a.c.geometric-sled-282503.internal>
2020-09-07 04:58:06 +0000

    edited main.tf

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -1,15 +1,14 @@
 module "academy-deploy" {
   source  = "fuchicorp/chart/helm"
+
   deployment_name        = "hello-world"
   deployment_environment = "${var.deployment_environment}"
   deployment_endpoint    = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}.${var.google_domain_name}"
   deployment_path        = "hello-world"
- 
-  template_custom_vars   = {     
-    deployment_image     = "${var.deployment_image}"
+  template_custom_vars  = {
+    deployment_image= "${var.deployment_image}"
    }
 }
-
 output "application_endpoint" {
     value = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}.${var.google_domain_name}"
 }
@@ -17,15 +16,15 @@ variable  "deployment_image" {
     default = "docker.bugdalorian.com/hello-world-app-dev-feature:f6858a7"
 }
 variable "deployment_environment" {
-    default = "qa"
+    default = "stage"
 }
 variable "deployment_endpoint" {
-    type      = "map"
-     default  = {
+    type = "map"
+     default = {
         test  = "test.hello"
-        dev   = "dev.hello"
-        qa    = "qa.hello"
-        prod  = "hello"
+        dev  = "dev.hello"
+        qa   = "qa.hello"
+        prod = "hello"
         stage = "stage.hello"
   }
 }
