a9298879c2e2407affb8840bcb12ae604b70ae1f
adnan kursun <Adnankursun@bastion-fuchicorp-com.us-central1-a.c.geometric-sled-282503.internal>
2020-09-09 07:06:02 +0000

    set-env file edited

--- deployments/terraform/set-env.sh
+++ deployments/terraform/set-env.sh
@@ -1,52 +1,29 @@
-#!/bin/bash -e 
+#!/usr/bin/env bash
 
-DATAFILE="$PWD/$1"
-#
-# FuchiCorp common script to set up Google terraform environment variables
-# these all variables should be created on your config file before you run script.
-# <ENVIRONMENT> <BUCKET> <DEPLOYMENT> <PROJECT> <CREDENTIALS>
+DIR=$(pwd)
+DATAFILE="$DIR/$1"
 
+BUCKET=$(sed -nr 's/^google_bucket_name\s*=\s*"([^"]*)".*$/\1/p'             "$DATAFILE")
+PROJECT=$(sed -nr 's/^google_project_id\s*=\s*"([^"]*)".*$/\1/p'             "$DATAFILE")
+ENVIRONMENT=$(sed -nr 's/^deployment_environment\s*=\s*"([^"]*)".*$/\1/p'    "$DATAFILE")
+DEPLOYMENT=$(sed -nr 's/^deployment_name\s*=\s*"([^"]*)".*$/\1/p'            "$DATAFILE")
+CREDENTIALS=$(sed -nr 's/^credentials\s*=\s*"([^"]*)".*$/\1/p'               "$DATAFILE") 
 
-PROJECT=$(sed -nr 's/^google_project_id\s*=\s*"([^"]*)".*$/\1/p'            "$DATAFILE")
-BUCKET=$(sed -nr 's/^google_bucket_name\s*=\s*"([^"]*)".*$/\1/p'            "$DATAFILE")
-ENVIRONMENT=$(sed -nr 's/^deployment_environment\s*=\s*"([^"]*)".*$/\1/p'   "$DATAFILE")
-DEPLOYMENT=$(sed -nr 's/^deployment_name\s*=\s*"([^"]*)".*$/\1/p'           "$DATAFILE")
-CREDENTIALS=$(sed -nr 's/^credentials\s*=\s*"([^"]*)".*$/\1/p'              "$DATAFILE")
 
 if [ ! -f "$DATAFILE" ]; then
-  echo "setenv: Configuration file not found: $DATAFILE"
-  return 1
-fi
-
-if [ -z "$PROJECT" ]
-then
-    echo "Inside <$DATAFILE> the <google_project_id> not found trying to find from <hello.tfvars>"
-    PROJECT=$(sed -nr 's/^google_project_id\s*=\s*"([^"]*)".*$/\1/p'  "$PWD/hello.tfvars")
-    echo "Using FuchiCorp Google Project ID for deployment. <google_project_id> : <$PROJECT>"
-fi
-
-if [ -z "$BUCKET" ]
-then
-  echo "Inside <$DATAFILE> the <google_bucket_name> not found trying to find from <hello.tfvars>"
-  BUCKET=$(sed -nr 's/^google_bucket_name\s*=\s*"([^"]*)".*$/\1/p'   "$PWD/hello.tfvars")
-  echo "Using FuchiCorp Google Bucket name for deployment. <google_bucket_name>: <$BUCKET>"
+    echo "setenv: Configuration file not found: $DATAFILE"
+    return 1
 fi
 
 if [ -z "$ENVIRONMENT" ]
 then
-    echo "setenv: 'deployment_environment' variable not set in configuration file."
+    echo "setenv: 'environment' variable not set in configuration file."
     return 1
 fi
 
 if [ -z "$BUCKET" ]
 then
-  echo "setenv: 'google_bucket_name' variable not set in configuration file."
-  return 1
-fi
-
-if [ -z "$PROJECT" ]
-then
-    echo "setenv: 'google_project_id' variable not set in configuration file."
+    echo "setenv: 'bucket_name' variable not set in configuration file."
     return 1
 fi
 
@@ -62,7 +39,13 @@ then
     return 1
 fi
 
-cat << EOF > "$PWD/backend.tf"
+if [ -z "$PROJECT" ]
+then
+    echo "setenv: 'google_project_id' variable not set in configuration file."
+    return 1
+fi
+
+cat << EOF > "$DIR/backend.tf"
 terraform {
   backend "gcs" {
     bucket  = "${BUCKET}"
@@ -71,12 +54,12 @@ terraform {
   }
 }
 EOF
-cat "$PWD/backend.tf"
+cat "$DIR/backend.tf"
 
-GOOGLE_APPLICATION_CREDENTIALS="${PWD}/${CREDENTIALS}"
+GOOGLE_APPLICATION_CREDENTIALS="${DIR}/${CREDENTIALS}"
 export GOOGLE_APPLICATION_CREDENTIALS
 export DATAFILE
-/bin/rm -rf "$PWD/.terraform" 2>/dev/null
-#/bin/rm -rf "$PWD/hello.tfvars" 2>/dev/null
+/bin/rm -rf "$DIR/.terraform" 2>/dev/null
+##/bin/rm -rf "$PWD/hello-world.tfvars" 2>/dev/null
 echo "setenv: Initializing terraform"
 terraform init #> /dev/null
